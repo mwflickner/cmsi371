18a85977227f6197688c22742bdbc882ea5a80f7
mflickner <mflickner@techempower.com>
2016-03-28 12:19:36 -0700

    started 0329b

---
+++ matrix-library/matrix.js
@@ -0,0 +1,30 @@
+var Matrix = class Matrix {
+	constructor(){
+		this.matrix = [
+					   [1,0,0,0],
+					   [0,1,0,0],
+					   [0,0,1,0],
+					   [0,0,0,1]
+					  ];
+	}
+
+	multiply(Matrix m){
+
+	}
+
+	scale(){
+
+	}
+
+	translate(){
+
+	}
+
+	rotate(){
+
+	}
+
+	equals(){
+		
+	}
+}
