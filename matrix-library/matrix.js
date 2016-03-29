39682f651516572a80c036e7e19b5d788820da58
Matthew <mwflickner@gmail.com>
2016-03-28 17:20:10 -0700

    messing around with es6

--- matrix-library/matrix.js
+++ matrix-library/matrix.js
@@ -1,10 +1,10 @@
 var Matrix = class Matrix {
-    constructor(){
+    constructor(x,y,z,tx,ty,tz){
         this.matrix = [
-                       [1,0,0,0],
-                       [0,1,0,0],
-                       [0,0,1,0],
-                       [0,0,0,1]
+                       [x, 0, 0, tx || 0],
+                       [0, y, 0, ty || 0],
+                       [0, 0, z, tz || 0],
+                       [0, 0, 0, 1]
                       ];
     }
 
@@ -13,13 +13,18 @@ var Matrix = class Matrix {
     }
 
     get scale(sx, sy, sz){
+        return new Matrix(sx,sy,sz);
+        /*
         x*sx, y*sy, z*sz
 
         (sx*x) + (0*y) + 0 --> sx  0  0
         (0*x) + (sy*y) + 0 --> 0  sy  0
+        */
     }
 
     get translate(tx, ty, tz){
+        return new Matrix(1,1,1,tx,ty,tz);
+        /*
         x + tx, y+ty, z + tz
 
         1*x + 0*x + tx --> 1  0  tx
@@ -34,6 +39,8 @@ var Matrix = class Matrix {
             -2  0 -10  |  x     -2x - 10
              0 -2  -4  |  y  =  -2y - 4
              0  0   1  |  1         1
+
+        */
     }   
 
     rotate(theta){
