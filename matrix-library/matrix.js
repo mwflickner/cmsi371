bb6e2381c104856d22c3430d14597054304f755b
Matthew <mwflickner@gmail.com>
2016-03-29 00:46:37 -0700

    refactoring matrixing library

--- matrix-library/matrix.js
+++ matrix-library/matrix.js
@@ -1,6 +1,80 @@
 (function(){
     window.Matrix = window.Matrix || {};
 
+    Matrix = function(){
+        this.elements = arguments.length ?
+            [].slice.call(arguments) :
+            [1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1];
+    };
+
+    Matrix.prototype.dimension = function (){
+        return this.elements.length
+    };
+
+    var multiplicationAllowed = function(a,b){
+        return a.dimension() === b.dimension();
+    };
+
+    Matrix.multiply = function(matrix){
+        var a = this;
+        var b = matrix;
+
+        if (!multiplicationAllowed(a,b)){
+            throw "Multiplication operation not allowed. Different dimensions";
+        } else {
+            /*
+                0   1   2   3     
+                4   5   6   7
+                8   9   10  11
+                12  13  14  15
+            */
+
+            return new Matrix(
+                    a[0]*b[0] + a[1]*b[4] + a[2]*b[8] + a[3]*b[12],
+                    a[0]*b[1] + a[1]*b[5] + a[2]*b[9] + a[3]*b[13],
+                    a[0]*b[2] + a[1]*b[6] + a[2]*b[10]+ a[3]*b[14],
+                    a[0]*b[3] + a[1]*b[7] + a[2]*b[11]+ a[3]*b[15],
+
+                    a[4]*b[0] + a[5]*b[4] + a[6]*b[8] + a[7]*b[12],
+                    a[4]*b[1] + a[5]*b[5] + a[6]*b[9] + a[7]*b[13],
+                    a[4]*b[2] + a[5]*b[6] + a[6]*b[10]+ a[7]*b[14],
+                    a[4]*b[3] + a[5]*b[7] + a[6]*b[11]+ a[7]*b[15],
+
+                    a[8]*b[0] + a[9]*b[4] + a[9]*b[8] + a[10]*b[12],
+                    a[8]*b[1] + a[9]*b[5] + a[9]*b[9] + a[10]*b[13],
+                    a[8]*b[2] + a[9]*b[6] + a[9]*b[10]+ a[10]*b[14],
+                    a[8]*b[3] + a[9]*b[7] + a[9]*b[11]+ a[10]*b[15],
+
+                    a[12]*b[0] + a[13]*b[4] + a[14]*b[8] + a[15]*b[12],
+                    a[12]*b[1] + a[13]*b[5] + a[14]*b[9] + a[15]*b[13],
+                    a[12]*b[2] + a[13]*b[6] + a[14]*b[10]+ a[15]*b[14],
+                    a[12]*b[3] + a[13]*b[7] + a[14]*b[11]+ a[15]*b[15]
+                );
+        }
+
+    }
+
+    Matrix.getTranslationMatrix = function(tx,ty,tz){
+        return new Matrix(
+                1,0,0,tx,
+                0,1,0,ty,
+                0,0,1,tz,
+                0,0,0,1
+            );
+    };
+
+    Matrix.getScaleMatrix = function(sx,sy,sz){
+        return new Matrix(
+                sx,0,0,0
+                0,sy,0,0
+                0,0,0,sz
+            );
+    };
+
+
 }());
 var Matrix = class Matrix {
     constructor(){
